a94a722b22b8f53425a9766f22971a9c9fb74dfa
David Owens II <owensd@kiadsoftware.com>
2014-08-23 14:43:51 -0700

    Added better printing and md5 crash sums for crashes; foundation for auto-validation of duplicate crashes.

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ swiftc_version=$(xcrun swiftc -version | cut -f2 -d"(" | cut -f1 -d")" | head -1
 
 echo
 echo "Running tests against: $swiftc_version"
-echo "    Using Xcode found at path: $xcode_path"
+echo "Using Xcode found at path: $xcode_path"
 
 num_crashed=0
 num_tests=0
@@ -14,6 +14,17 @@ color_red="\e[01;31m"
 color_green="\e[32m"
 color_stop="\e[00m"
 
+columns=$(tput cols)
+
+#
+# Test results should be logged as this (+---...---+ is the width of terminal):
+# +------------------------------------------------------------+
+#   ✘  <test name>                                (88591ef1ad)
+#
+
+name_size=$((columns-20))
+format_fail="  %b  %-$name_size.${name_size}b (%10.10b)\n"
+format_pass="  %b  %-$name_size.${name_size}b\n"
 
 do_test() {
   path="$1"
@@ -23,13 +34,16 @@ do_test() {
     test_name=$(basename -s ".swift" "$path")
     test_name=${test_name//-/ }
     
-    $(xcrun swiftc $path 2>&1 | egrep -q "error: unable to execute command: Segmentation fault:")
-
+    output=$(xcrun swiftc $path 2>&1)
+    
+    md5=$(echo $output | md5)
+    
+    $(echo $output | egrep -q "error: unable to execute command: Segmentation fault:")
     if [ $? == 0 ]; then
       num_crashed=$((num_crashed + 1))
-      printf "    ${color_red}✘${color_stop}  $test_name\n"
+      printf "$format_fail" "${color_red}✘${color_stop}" "$test_name" "$md5"
     else
-      printf "    ${color_green}✓${color_stop}  $test_name\n"
+      printf "$format_pass" "${color_green}✓${color_stop}" "$test_name" 
     fi
   fi
 }
