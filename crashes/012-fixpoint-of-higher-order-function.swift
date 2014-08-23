c50a535fe572132ce3b5a5ae0323ccb5c1613594
practicalswift <practicalswift@users.noreply.github.com>
2014-08-23 11:41:17 +0200

    Minimized test case.

--- crashes/012-fixpoint-of-higher-order-function.swift
+++ crashes/012-fixpoint-of-higher-order-function.swift
@@ -1,4 +1,5 @@
-func boom<T>(f: ((T, T -> T) -> T, T, (T -> T)) -> T) -> (T, T -> T) -> T {
-	var recursive: ((T, T -> T) -> T)!
-	return recursive
+func a<T>() -> (T, T -> T) -> T {
+    var b: ((T, T -> T) -> T)!
+    return b
 }
+
