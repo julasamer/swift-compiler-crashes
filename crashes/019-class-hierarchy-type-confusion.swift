5cb2f49647076d946cdfe177f43ae28fc6b156c8
practicalswift <practicalswift@users.noreply.github.com>
2014-08-23 20:57:59 +0200

    Added test case 019-class-hierarchy-type-confusion.swift

---
+++ crashes/019-class-hierarchy-type-confusion.swift
@@ -0,0 +1,23 @@
+protocol A {
+    func c() -> String
+}
+
+class B {
+    func d() -> String {
+        return ""
+    }
+}
+
+class C: B, A {
+    override func d() -> String {
+        return ""
+    }
+
+    func c() -> String {
+        return ""
+    }
+}
+
+func e<T where T: A, T: B>(t: T) {
+    let g = t.c() + t.d()
+}
