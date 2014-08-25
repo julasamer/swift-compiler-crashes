87744ad4d1f265370929a09d3c2c073f44b709c3
practicalswift <practicalswift@users.noreply.github.com>
2014-08-25 19:04:50 +0200

    Added test case 026-extension-property-referenced-from-another-file.part{1,2}.swift

---
+++ crashes/026-extension-property-referenced-from-another-file.part1.swift
@@ -0,0 +1,11 @@
+// https://twitter.com/krzyzanowskim/status/501658160428367872
+// https://github.com/krzyzanowskim/CryptoSwift/commit/c8e779f9af5d9fe1debdfcfe45803ecbf3d61fc2
+// rdar://18061554
+
+import Foundation
+
+extension NSData {
+    public var a: String {
+        return ""
+    }
+}
