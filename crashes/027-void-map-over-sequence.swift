0ff97ab874f1a35149e965020f3a35ec6bd65989
practicalswift <practicalswift@users.noreply.github.com>
2014-08-26 09:32:06 +0200

    Minimized test case further.

--- crashes/027-void-map-over-sequence.swift
+++ crashes/027-void-map-over-sequence.swift
@@ -1,14 +1,10 @@
 // rdar://18118173
 
-class Set<T: Hashable> {
+class a<T : Hashable> {
     typealias Element = T
-    var contents : [Element: Bool]
-
-    init<S: SequenceType where S.Generator.Element == Element>(_ s : S) {
-        self.contents = [Element: Bool]()
-        // Note: it would be more idiomatic to use for-in, but that doesn't
-        //       compile in Beta-6.
-        map(s) { self.contents[$0] = true; }
+    var b = [Element: Bool]()
+    init<S: SequenceType where S.Generator.Element == Element>(_ s: S) {
+        map(s) { self.b[$0] = true }
     }
 }
-Set([1,2,3])
+a([1])
