35d703c43e529439db1f983d9a0aec98232380e6
practicalswift <practicalswift@users.noreply.github.com>
2014-08-26 09:33:24 +0200

    Minimized further.

--- crashes/027-void-map-over-sequence.swift
+++ crashes/027-void-map-over-sequence.swift
@@ -1,9 +1,8 @@
 // rdar://18118173
 
 class a<T : Hashable> {
-    typealias Element = T
-    var b = [Element: Bool]()
-    init<S: SequenceType where S.Generator.Element == Element>(_ s: S) {
+    var b = [T: Bool]()
+    init<S: SequenceType where S.Generator.Element == T>(_ s: S) {
         map(s) { self.b[$0] = true }
     }
 }
