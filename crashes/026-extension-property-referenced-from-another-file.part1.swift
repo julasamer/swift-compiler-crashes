97818a8e2d6b1715bc506d6253611284166c2efa
practicalswift <practicalswift@users.noreply.github.com>
2014-08-26 16:59:12 +0200

    Added note about similarity between #026 and #028.

--- crashes/026-extension-property-referenced-from-another-file.part1.swift
+++ crashes/026-extension-property-referenced-from-another-file.part1.swift
@@ -1,6 +1,7 @@
 // https://twitter.com/krzyzanowskim/status/501658160428367872
 // https://github.com/krzyzanowskim/CryptoSwift/commit/c8e779f9af5d9fe1debdfcfe45803ecbf3d61fc2
 // rdar://18061554
+// NOTE: This bug is similar (but not identical) to #028.
 
 import Foundation
 
