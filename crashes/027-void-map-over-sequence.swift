d40ff26bdb5a61b53a6190f9f5f0efd32dea8a90
practicalswift <practicalswift@users.noreply.github.com>
2014-08-26 09:46:09 +0200

    Normalized test case to conform to formatting of other cases.

--- crashes/027-void-map-over-sequence.swift
+++ crashes/027-void-map-over-sequence.swift
@@ -1,9 +1,9 @@
 // rdar://18118173
 
 class a<T : Hashable> {
-    var b = [T: Bool]()
-    init<S: SequenceType where S.Generator.Element == T>(_ s: S) {
-        map(s) { self.b[$0] = true }
+    var b = [T : Bool]()
+    init<S : SequenceType where S.Generator.Element == T>(_ c: S) {
+        map(c) { self.b[$0] = true }
     }
 }
 a([1])
