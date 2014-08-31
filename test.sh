221ce44292e6e7e0a772b0f72ebec2cb8d80c4ad
practicalswift <practicalswift@users.noreply.github.com>
2014-08-31 20:52:21 +0200

    Now testing timeout tests before other tests to avoid hanging the test runner.

--- test.sh
+++ test.sh
@@ -43,8 +43,10 @@ seen_checksums=""
 execute_with_timeout() {
   timeout_in_seconds=$1
   command=$2
-  expect -c "set echo \"-noecho\"; set timeout $timeout_in_seconds; spawn -noecho /bin/sh -c \"${command}\"; expect timeout { exit 1 } eof { exit 0 } > /dev/null 2> /dev/null"
-  return $?
+  out=$(expect -c "set echo \"-noecho\"; set timeout $timeout_in_seconds; spawn -noecho /bin/sh -c \"${command}\"; expect timeout { exit 1 } eof { exit 0 }")
+  return_code=$?
+  echo "${out}" | tr -d "\r"
+  return $return_code
 }
 
 test_file() {
@@ -66,13 +68,24 @@ test_file() {
   test_name=${test_name//.script/}
   test_name=${test_name//.timeout/}
   swift_crash=0
-  # Test mode 1. Compile using swiftc without any optimizations ("-Onone").
-  output=$(xcrun swiftc -Onone -o /dev/null ${files_to_compile} 2>&1)
-  if [[ ${output} =~ (error:\ unable\ to\ execute\ command:\ Segmentation\ fault:|LLVM\ ERROR:|While\ emitting\ IR\ for\ source\ file) ]]; then
-    swift_crash=1
-  fi
   compilation_comment=""
-  # Test mode 2. Compile using swiftc with optimization option "-O".
+  # Test mode 1. Run Swift code and catch a portential hang (infinite running time).
+  #              Used for test cases named *.timeout.swift.
+  if [[ ${swift_crash} == 0 && ${files_to_compile} =~ \.timeout\. ]]; then
+    execute_with_timeout 5 "xcrun swift ${files_to_compile}" > /dev/null 2> /dev/null
+    if [[ $? == 1 ]]; then
+      swift_crash=1
+      compilation_comment="timeout"
+    fi
+  fi
+  # Test mode 2. Compile using swiftc without any optimizations ("-Onone").
+  if [[ ${swift_crash} == 0 ]]; then
+    output=$(xcrun swiftc -Onone -o /dev/null ${files_to_compile} 2>&1)
+    if [[ ${output} =~ (error:\ unable\ to\ execute\ command:\ Segmentation\ fault:|LLVM\ ERROR:|While\ emitting\ IR\ for\ source\ file) ]]; then
+      swift_crash=1
+    fi
+  fi
+  # Test mode 3. Compile using swiftc with optimization option "-O".
   if [[ ${swift_crash} == 0 ]]; then
     output=$(xcrun swiftc -O -o /dev/null ${files_to_compile} 2>&1)
     compilation_comment="-O"
@@ -80,7 +93,7 @@ test_file() {
       swift_crash=1
     fi
   fi
-  # Test mode 3. Compile with file #1 as a library and file #2 as a library user.
+  # Test mode 4. Compile with file #1 as a library and file #2 as a library user.
   #              Used for test cases named *.library{1,2}.swift.
   if [[ ${swift_crash} == 0 && ${files_to_compile} =~ \.library1\. && -f ${files_to_compile//.library1./.library2.} ]]; then
     source_file_using_library=${files_to_compile//.library1./.library2.}
@@ -98,16 +111,7 @@ test_file() {
     fi
     rm -f DummyModule.swiftdoc DummyModule.swiftmodule libDummyModule.dylib
   fi
-  # Test mode 4. Run Swift code and catch a portential hang (infinite running time).
-  #              Used for test cases named *.timeout.swift.
-  if [[ ${swift_crash} == 0 && ${files_to_compile} =~ \.timeout\. ]]; then
-    execute_with_timeout 5 "xcrun swift ${files_to_compile}"
-    if [[ $? == 1 ]]; then
-      swift_crash=1
-      compilation_comment="timeout"
-    fi
-  fi
-  # Test mode 4. Run Swift code both using -Onone and -O and watch for differences.
+  # Test mode 5. Run Swift code both using -Onone and -O and watch for differences.
   #              Used for test cases named *.script.swift.
   if [[ ${swift_crash} == 0 && ${files_to_compile} =~ \.script\. ]]; then
     output_1=$(xcrun swift -Onone ${files_to_compile} 2>&1)
