b119c7f107a5bcc87f4e0cb1af72f932a4505a72
practicalswift <practicalswift@users.noreply.github.com>
2014-09-06 00:16:11 +0200

    Added test case 048-unowned-self-broken-if-inheriting-nsobject.runtime.swift

---
+++ crashes/048-unowned-self-broken-if-inheriting-nsobject.runtime.swift
@@ -0,0 +1,29 @@
+// Test case submitted to project by https://github.com/practicalswift (practicalswift)
+// https://twitter.com/nuoji/status/507991706294558721
+// https://gist.github.com/lerno/4cb4568c53045aafb66e
+
+import Foundation
+
+class A : NSObject {
+    var c: (()->Bool)?
+    deinit {
+        println("A")
+    }
+}
+
+class B : NSObject {
+    var d = A()
+    override init() {
+        super.init()
+        d.c = {
+            [unowned self] in
+            println("\(self)")
+            return true
+        }
+    }
+}
+
+var e: B? = B()
+var f: A? = e!.d
+e = nil
+f = nil
