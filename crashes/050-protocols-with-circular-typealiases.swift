4470ec2aa5e50c20b77f9cdd3f1aa2749a13ce8c
practicalswift <practicalswift@users.noreply.github.com>
2014-09-07 22:28:07 +0200

    Added test case 050-protocols-with-circular-typealiases.swift

---
+++ crashes/050-protocols-with-circular-typealiases.swift
@@ -0,0 +1,11 @@
+// Test case submitted to project by https://github.com/practicalswift (practicalswift)
+// http://www.openradar.me/17986597
+// Similar to 010-circular-protocol-reference.swift, might be same underlying bug.
+
+protocol A {
+    typealias A = B
+}
+
+protocol B {
+    typealias B = A
+}
