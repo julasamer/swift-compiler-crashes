f71e018fed0a257f268f4d5ac7114ce026715e79
practicalswift <practicalswift@users.noreply.github.com>
2014-09-07 11:22:53 +0200

    Updated test case.

--- crashes/049-type-inference-with-closure-expression.swift
+++ crashes/049-type-inference-with-closure-expression.swift
@@ -5,11 +5,10 @@ var f1: Int -> Int = {
     return $0
 }
 
-// Succeeds
-let y = { (x: Int, f: Int -> Int) -> Int in
+let succeeds: Int = { (x: Int, f: Int -> Int) -> Int in
     return f(x)
 }(x1, f1)
 
-let fail: Int = { x, f in
+let crashes: Int = { x, f in
     return f(x)
 }(x1, f1)
